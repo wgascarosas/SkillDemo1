6b728305184f1568595ae4b14a983d9b956c583b
wgascarosas <97696374+wgascarosas@users.noreply.github.com>
2022-02-04 10:28:01 -0800

    commit w failures

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,1 +1,7 @@
 //some code
+
+public class SkillDemo1{
+    public static int mult(int a, int b){
+        return a * b;
+    }
+}
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void multiply() {
+        assertEquals(50, SkillDemo1.mult(3, 9));
+    }
+}
